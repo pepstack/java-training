52f1d1f87eee45351ef4fb8d7eb4c4dc4833472d
cheungmine <350137278@qq.com>
2019-09-20 12:01:54 +0800

    add webapp

---
+++ sb2-demo/src/main/java/com/pepstack/sb2demo/Sb2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.pepstack.sb2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb2DemoApplication.class, args);
+	}
+
+}
